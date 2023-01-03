5f31abfd34f12cb3bb197f35bd875669a4dab89d
sam625232 <89412697+sam625232@users.noreply.github.com>
2023-01-03 02:47:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.prepare().then(() => {
     return handle(req, res);
   });
 
-  server.listen(3000, (err) => {
+  server.listen(process.env.PORT, (err) => {
     if (err) throw err;
     console.log("> Ready on http://localhost:3000");
   });
